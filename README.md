a8a22afc9b1ed1d989bfb70c1c34feeed766bd46
Edisel Navas Conyedo <enavas@uci.cu>
2024-12-19 21:39:12 -0500

    Basic Structures

---
+++ README.md
@@ -0,0 +1,1 @@
+# FixedFloat API Manager
--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='pyfixedfloat',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+       ' requests'
+        # Aquí puedes listar las dependencias, por ejemplo:
+        # 'numpy',
+    ],
+    author='Ertytux Tux',
+    author_email='hivep2p@gmail.com',
+    description='Paquete para el manejo de FixedFloat',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Ertytux/pyfixedfloat',  # URL del repositorio
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+    ],
+)
