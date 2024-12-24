8d34d4f9ecb4cb7ccb00f2e866750184784a17c9
Edisel Navas Conyedo <enavas@uci.cu>
2024-12-23 20:39:49 -0500

    Add test basic

--- test/ffioTest.py
+++ test/ffioTest.py
@@ -1,1 +1,12 @@
+from ffio.ffinit import FFio
+import os
 
+#Get actives coins
+apikey=os.getenv('ffapi')
+seckey=os.getenv('ffsec')
+refcode='va9kxaja'
+afftax=0.3
+
+ff=FFio(apikey=apikey,seckey=seckey,refcode=refcode,stdata=afftax)
+
+print(ff.coins())
